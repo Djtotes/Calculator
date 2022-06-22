8dc712a7fe1e8bb40dae9cc6b09045882fbe1047
Djtotes <107389380+Djtotes@users.noreply.github.com>
2022-06-22 20:32:32 +0100

    Uploaded

---
+++ about.js
@@ -0,0 +1,5 @@
+var myName = "Okorie Success Uchechukwu ";
+var height = "1.95 metres";
+var country = "Nigeria";
+alert("My name is " + myName + ", I am " + height + " tall, "+ "I am from " + country);
+console.log("My name is " + myName + ", I am " + height + " tall, "+ "I am from " + country);
